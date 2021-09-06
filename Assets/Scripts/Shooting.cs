c54b7ad9e356dce201e19cfb87dd879d2031e546
YarinDev <yarin.software@gmail.com>
2021-09-06 13:25:27 +0300

    v.0.3.5.9
    - added shooting hits and sounds
    - added knight stumble at shooting and dying getting 3 shots
    - added muzzle effect

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -8,11 +8,20 @@ public class Shooting : MonoBehaviour
 
     public GameObject target;
 
-    //public GameObject muzzle;
+    public GameObject muzzle;
+
+    private LineRenderer line;
+    public AudioSource shootingSound;
+    public GameObject Knight;
+    private Animator animator;
+    private static int numHits;
 
     // Start is called before the first frame update
     void Start()
     {
+        numHits = 0;
+        line = GetComponent<LineRenderer>();
+        animator = Knight.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,7 +33,49 @@ public class Shooting : MonoBehaviour
             if (Physics.Raycast(aCamera.transform.position, aCamera.transform.forward, out hit))
             {
                 target.transform.position = hit.point;
+                //draw line
+                shootingSound.Play();
+                StartCoroutine(ShowFlash());
+
+
+                //check that the bullet hit the knight
+                if (hit.transform.gameObject == Knight)
+                {
+                    numHits++;
+                    if (numHits<3)
+                    {
+                        StartCoroutine(KnightFallAndGettingUp());
+
+                    }
+                    else
+                    {
+                        animator.SetInteger("state", 4); //dying
+
+                    }
+                }
             }
         }
+    }
+
+    IEnumerator KnightFallAndGettingUp()
+    {
+        animator.SetInteger("state", 2); //fall back
+        yield return new WaitForSeconds(2f); // delay
+        animator.SetInteger("state", 3); //getting up
+    }
+    
+
+    IEnumerator ShowFlash()
+    {
+        //draw flash
+        line.SetPosition(0, muzzle.transform.position);
+        line.SetPosition(1, target.transform.position);
+
+        //all the lines before next run immidiatly
+        yield return new WaitForSeconds(0.1f); // delay
+        //the next line will be executed after the delay
+        //erase flash
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, transform.position);
     }
 }
